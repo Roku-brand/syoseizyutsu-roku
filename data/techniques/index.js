4db392a207d38eb7b74904497c5f3d6656482e9f
RoKu-Brand <tsubasa00928@gmail.com>
2026-01-24 21:36:31 +0900

    Fix technique card exports

--- data/techniques/index.js
+++ data/techniques/index.js
@@ -1,9 +1,9 @@
 import { achievementCards } from "./achievement.js";
-import { lifeCards } from "./life.js";
-import { peopleCardsPart1 } from "./people-1.js";
-import { peopleCardsPart2 } from "./people-2.js";
-import { skillCards } from "./skill.js";
-import { thinkingCards } from "./thinking.js";
+import { lifeTechniques as lifeCards } from "./life.js";
+import { peopleTechniquesPart1 as peopleCardsPart1 } from "./people-1.js";
+import { peopleTechniquesPart2 as peopleCardsPart2 } from "./people-2.js";
+import { skillTechniques as skillCards } from "./skill.js";
+import { thinkingTechniques as thinkingCards } from "./thinking.js";
 
 export const techniqueCards = {
   achievement: achievementCards,
